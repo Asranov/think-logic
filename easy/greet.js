ac4ce56c90dc8c4574a4556e76e97d3f955669a6
Asranov <asranov0003@gmail.com>
2024-05-14 20:36:12 +0500

    feat: add greet

---
+++ easy/greet.js
@@ -0,0 +1,10 @@
+// Returning Strings
+
+// Write a function that given the input string name, returns the greeting statement Hello, <name> how are you doing today?
+
+const greet = (name) => {
+  return `Hello, ${name} how are you doing today?`;
+};
+
+console.log(greet("Ryan")); // "Hello, Ryan how are you doing today?"
+console.log(greet("Sara")); // "Hello, Sara how are you doing today?"
