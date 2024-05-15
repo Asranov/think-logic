26f9d4da323540d9dda92a2091a1fdecd2b0d812
Asranov <asranov0003@gmail.com>
2024-05-15 22:04:21 +0500

    feat: add is divisible

---
+++ easy/isDivisible.js
@@ -0,0 +1,13 @@
+// Is n Divisible by x and y?
+
+// Create a function that checks if a number n is divisible by two numbers x AND y.
+// All inputs are positive, non-zero digits.
+
+const isDivisible = (n, x, y) => {
+  return n % x === 0 && n % y === 0;
+};
+
+console.log(isDivisible(3, 3, 4)); // false
+console.log(isDivisible(12, 3, 4)); // true
+console.log(isDivisible(8, 3, 4)); // false
+console.log(isDivisible(48, 3, 4)); // true
