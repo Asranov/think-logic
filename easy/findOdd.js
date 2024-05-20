e3b645999af7e727a63207a310c6d2f9b9e14794
Asranov <asranov0003@gmail.com>
2024-05-20 17:56:02 +0500

    feat: add find odd

---
+++ easy/findOdd.js
@@ -0,0 +1,13 @@
+// Find the Odd Int
+// Given an array of integers, find the one that appears an odd number of times. There will always be only one integer that appears an odd number of times.
+
+const findOdd = (numbers) => {
+  return numbers.reduce((a, b) => a ^ b);
+};
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])); // 5
+console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5])); // 5
+console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5])); // -1
+console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10])); // 1
+console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1])); // 10
+console.log(findOdd([10])); // 10
